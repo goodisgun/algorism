f1cf16f9524dce946454be11255453536e8f01bc
EunchongJo <agapejo456@naver.com>
2022-12-07 20:25:11 +0900

    1일차 알고리즘

---
+++ oneweek/oneday.java
@@ -0,0 +1,31 @@
+package oneweek;
+
+public class oneday {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            if (n % 7 ==0){
+                answer = n / 7;
+            } else if(n % 7 !=0){
+                answer = n / 7 + 1;
+            } return answer;
+        }
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        if (n <= 7  ) {
+            answer = 1;
+        } else if (8 <= n && n < 15){
+            answer = 2;
+        }else if (15 <= n & n < 22){
+            answer = 3;
+        } return answer;
+
+    }
+}
+
+
---
+++ oneweek/twoday.java
@@ -0,0 +1,17 @@
+package oneweek;
+
+// 나머지가 0 = 배수로 판단할 수 있다.
+// 향상된 for문을 사용하여 num변수를 추출하면 numlist값이 순서대로 출력된다 * 단 if문 통과시에
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> solution(int n, int[] numlist) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int num : numlist){
+            if (num % n ==0){
+                answer.add(num);
+            }
+        }
+        return answer;
+    }
+}
